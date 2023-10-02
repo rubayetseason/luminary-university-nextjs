39fc2c1eb00e770f44f952e142827139f886b535
rubayet-islam <season.ss2001@gmail.com>
2023-10-02 21:22:35 +0600

    reusable stepper form

--- src/app/(withDashboardLayout)/super_admin/manage-student/create-student/page.tsx
+++ src/app/(withDashboardLayout)/super_admin/manage-student/create-student/page.tsx
@@ -1,7 +1,30 @@
+import StepperForm from "@/components/forms/StepperForm";
+import StudentInfo from "@/components/studentForm/StudentInfo";
+
 const CreateStudentRoute = () => {
+  const steps = [
+    {
+      title: "Student Information",
+      content: <StudentInfo />,
+    },
+    {
+      title: "Basic Information",
+      content: "Second-content",
+    },
+    {
+      title: "Guardian Information",
+      content: "Last-content",
+    },
+    {
+      title: "Local Guardian Information",
+      content: "Last-content",
+    },
+  ];
+
   return (
     <div>
-      <h1>This is create student route</h1>
+      <h1 style={{ marginBottom: "10px" }}>Create student</h1>
+      <StepperForm steps={steps}></StepperForm>
     </div>
   );
 };
---
+++ src/components/studentForm/StudentInfo.tsx
@@ -0,0 +1,10 @@
+
+const StudentInfo = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default StudentInfo;
