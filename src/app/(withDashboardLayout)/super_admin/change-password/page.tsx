0e327921c73b8030a924a4c75bf6299fc19a45d9
rubayet-islam <season.ss2001@gmail.com>
2023-10-03 20:26:46 +0600

    change password form

--- src/app/(withDashboardLayout)/super_admin/change-password/page.tsx
+++ src/app/(withDashboardLayout)/super_admin/change-password/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import Form from "@/components/forms/Form";
+import FormInput from "@/components/forms/FormInput";
 import BreadCrumb from "@/components/ui/BreadCrumb";
 import { getUserInfo } from "@/services/auth.services";
+import { Button } from "antd";
 
 const ChangePasswordRoute = () => {
   const { role } = getUserInfo() as any;
+
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <BreadCrumb
@@ -19,7 +31,30 @@ const ChangePasswordRoute = () => {
           },
         ]}
       />
-      <h1>this is change password</h1>
+      <div
+        style={{ margin: "100px 0", display: "flex", justifyContent: "center" }}
+      >
+        <Form submitHandler={onSubmit}>
+          <h2 style={{ marginBottom: "10px" }}>Reset Password</h2>
+          <div style={{ margin: "5px 0" }}>
+            <FormInput
+              name="oldPassword"
+              label="Old password"
+              type="password"
+            />
+          </div>
+          <div style={{ margin: "5px 0" }}>
+            <FormInput
+              name="newPassword"
+              label="New password"
+              type="password"
+            />
+          </div>
+          <Button type="primary" htmlType="submit">
+            Change password
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
