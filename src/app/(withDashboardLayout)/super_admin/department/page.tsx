d9f65142bc1122dfe928299ac3c193ee624d5dda
rubayet-islam <season.ss2001@gmail.com>
2023-10-09 19:11:44 +0600

    added conditional rendering

--- src/app/(withDashboardLayout)/super_admin/department/page.tsx
+++ src/app/(withDashboardLayout)/super_admin/department/page.tsx
@@ -118,13 +118,12 @@ const ManageDepartmentRoute = () => {
           style={{ width: "60%" }}
           onChange={(e) => setSearchTerm(e.target.value)}
         ></Input>
-        {sortOrder ||
-          sortBy ||
-          (searchTerm && (
-            <Button onClick={resetFilters} danger style={{ margin: "0px 5px" }}>
-              <ReloadOutlined />
-            </Button>
-          ))}
+
+        {(sortBy || sortOrder || searchTerm) && (
+          <Button onClick={resetFilters} danger style={{ margin: "0px 5px" }}>
+            <ReloadOutlined />
+          </Button>
+        )}
       </div>
       {/* department table */}
       <div>
