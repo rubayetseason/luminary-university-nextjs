4a978e3c5f4c42c5100cada3a73bbbdf7d202f58
rubayet-islam <season.ss2001@gmail.com>
2023-10-09 20:51:53 +0600

    admin display reusable table

--- src/app/(withDashboardLayout)/super_admin/admin/page.tsx
+++ src/app/(withDashboardLayout)/super_admin/admin/page.tsx
@@ -1,12 +1,140 @@
 "use client";
 
 import BreadCrumb from "@/components/ui/BreadCrumb";
+import { useDebounced } from "@/hooks/hooks";
+import { useAdminsQuery } from "@/redux/api/adminApi";
 import { getUserInfo } from "@/services/auth.services";
+import { IManageDepartments } from "@/types";
 import { Button } from "antd";
 import Link from "next/link";
+import { useState } from "react";
+import dayjs from "dayjs";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ReloadOutlined,
+  EyeOutlined,
+} from "@ant-design/icons";
+import ReusableTable from "@/components/ui/ReusableTable";
 
 const ManageAdminRoute = () => {
   const { role } = getUserInfo() as any;
+
+  const query: Record<string, any> = {};
+
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+
+  const debouncedSearchTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 1000,
+  });
+
+  if (!!debouncedSearchTerm) {
+    query["searchTerm"] = debouncedSearchTerm;
+  }
+  const { data, isLoading } = useAdminsQuery({ ...query });
+
+  const admins = data?.admins;
+  const meta = data?.meta;
+
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      sorter: true,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: function (data: Record<string, string>) {
+        const fullName = `${data?.firstName} ${data?.middleName} ${data?.lastName}`;
+        return <>{fullName}</>;
+      },
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Department",
+      dataIndex: "managementDepartment",
+      render: function (data: IManageDepartments) {
+        return <>{data?.title}</>;
+      },
+    },
+    {
+      title: "Designation",
+      dataIndex: "designation",
+    },
+    {
+      title: "Created at",
+      dataIndex: "createdAt",
+      render: function (data: any) {
+        return data && dayjs(data).format("MMM D, YYYY hh:mm A");
+      },
+      sorter: true,
+    },
+    {
+      title: "Contact no.",
+      dataIndex: "contactNo",
+    },
+    {
+      title: "Action",
+      dataIndex: "id",
+      render: function (data: any) {
+        return (
+          <>
+            <Link href={`/super_admin/admin/details/${data.id}`}>
+              <Button onClick={() => console.log(data)} type="primary">
+                <EyeOutlined />
+              </Button>
+            </Link>
+            <Link href={`/super_admin/admin/edit/${data.id}`}>
+              <Button
+                style={{
+                  margin: "0px 5px",
+                }}
+                onClick={() => console.log(data)}
+                type="primary"
+              >
+                <EditOutlined />
+              </Button>
+            </Link>
+            <Button onClick={() => console.log(data)} type="primary" danger>
+              <DeleteOutlined />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+    setPage(page);
+    setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
+  const resetFilters = () => {
+    setSortBy("");
+    setSortOrder("");
+    setSearchTerm("");
+  };
+
   return (
     <div>
       <BreadCrumb
@@ -21,10 +149,24 @@ const ManageAdminRoute = () => {
           },
         ]}
       />
-      <h1 style={{ margin: "10px 0" }}>Create admin</h1>
+      <h1 style={{ margin: "10px 0" }}>Admin List</h1>
       <Link href="/super_admin/admin/create-admin">
         <Button type="primary">Create admin</Button>
       </Link>
+      <div style={{ margin: "10px 0px" }}>
+        {" "}
+        <ReusableTable
+          loading={isLoading}
+          columns={columns}
+          dataSource={admins}
+          pageSize={size}
+          totalPages={meta?.total}
+          showSizeChanger={true}
+          onPaginationChange={onPaginationChange}
+          onTableChange={onTableChange}
+          showPagination={true}
+        />
+      </div>
     </div>
   );
 };
